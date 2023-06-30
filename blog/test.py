a32a492186f7fe351975ec619973d4036a75b6c0
ggjp <54991419+ggjp@users.noreply.github.com>
2023-06-30 17:23:52 +0900

    Update test.py

--- blog/test.py
+++ blog/test.py
@@ -1,33 +1,22 @@
-Sub SplitAndReposition()
-    Dim ws As Worksheet
-    Dim rng As Range
-    Dim cell As Range
-    Dim splitValues As Variant
-    Dim i As Integer
-    Dim lastRow As Long
-    Dim output As String
+<ee:transform doc:name="Transform Message" >
+    <ee:message>
+        <ee:set-payload><![CDATA[%dw 2.0
+            output application/java
+            ---
+            {
+                'tenpo_cd': '\'' ++ payload.tenpo_cd ++ '\'',
+                'prd_cd': payload.prd_cd map '\'' ++ $ ++ '\'' joinBy ','
+            }]]></ee:set-payload>
+    </ee:message>
+</ee:transform>
 
-    Set ws = ThisWorkbook.Sheets("Sheet1")  ' シート名を適切に変更してください
-    lastRow = ws.Cells(ws.Rows.Count, "B").End(xlUp).Row  ' B列の最終行を取得
-
-    Set rng = ws.Range("B1:B" & lastRow)  ' B列にデータが入力されているセルを対象にする
-
-    ' 範囲内の各セルに対して処理
-    For Each cell In rng
-        If cell.Value <> "" Then  ' B列のセルにデータが入力されている場合だけ処理を行う
-            splitValues = Split(cell.Value, vbLf)
-
-            output = ""
-            For i = LBound(splitValues) To UBound(splitValues)
-                ' 改行で分割したテキストをカンマで結合
-                output = output & splitValues(i) & ","
-            Next i
-
-            ' 末尾の余分なカンマを削除
-            output = Left(output, Len(output) - 1)
-
-            ' 結果をL列に出力
-            ws.Cells(cell.Row, "L").Value = output
-        End If
-    Next cell
-End Sub
+<db:select doc:name="Select" doc:id="8d88c75a-3062-4b33-bc6a-43f800aa0cb3" config-ref="Database_Config">
+    <db:sql>
+        <![CDATA[
+            SELECT * FROM products WHERE tenpo_cd = :tenpo_cd AND product_code IN (:prd_cd)
+        ]]>
+    </db:sql>
+    <db:input-parameters>
+        #[payload]
+    </db:input-parameters>
+</db:select>
