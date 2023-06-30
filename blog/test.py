eec87995d697b263a06e8c6aac244cb53c396b8d
ggjp <54991419+ggjp@users.noreply.github.com>
2023-06-30 09:20:08 +0900

    Update test.py

--- blog/test.py
+++ blog/test.py
@@ -1,17 +1,25 @@
 Sub SplitAndReposition()
+    Dim ws As Worksheet
     Dim rng As Range
     Dim cell As Range
     Dim splitValues As Variant
     Dim i As Integer
-    
-    ' 変換したいセルを指定
-    Set rng = Range("A1")
-    
-    ' 改行でテキストを分割
-    splitValues = Split(rng.Value, vbLf)
-    
-    ' 分割したテキストを列に展開
-    For i = LBound(splitValues) To UBound(splitValues)
-        rng.Offset(0, i).Value = splitValues(i)
-    Next i
+    Dim lastRow As Long
+
+    Set ws = ThisWorkbook.Sheets("Sheet1")  ' シート名を適切に変更してください
+    lastRow = ws.Cells(ws.Rows.Count, "B").End(xlUp).Row  ' B列の最終行を取得
+
+    Set rng = ws.Range("B1:B" & lastRow)  ' B列にデータが入力されているセルを対象にする
+
+    ' 範囲内の各セルに対して処理
+    For Each cell In rng
+        If cell.Value <> "" Then  ' B列のセルにデータが入力されている場合だけ処理を行う
+            splitValues = Split(cell.Value, vbLf)
+
+            For i = LBound(splitValues) To UBound(splitValues)
+                ' コピー先をL列に設定
+                ws.Cells(cell.Row, "L").Offset(0, i).Value = splitValues(i)
+            Next i
+        End If
+    Next cell
 End Sub
