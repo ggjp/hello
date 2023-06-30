f71b7f411b6e3043851712a174e37dc109002ff5
ggjp <54991419+ggjp@users.noreply.github.com>
2023-06-30 09:07:55 +0900

    Update test.py

--- blog/test.py
+++ blog/test.py
@@ -1,16 +1,17 @@
-<http:listener config-ref="HTTP_Listener_config" path="/yourApiPath" allowedMethods="POST" doc:name="HTTP Listener" />
-<set-variable variableName="tenpo_cd" value="#[payload.tenpo_cd]" doc:name="Set Variable (tenpo_cd)" />
-<set-variable variableName="prd_cds" value="#[payload.prd_cds]" doc:name="Set Variable (prd_cds)" />
-<ee:transform doc:name="Prepare Query" >
-    <ee:message >
-        <ee:set-payload ><![CDATA[%dw 2.0
-            output application/java
-            ---
-            "SELECT * FROM tableName WHERE tenpo_cd = '" ++ vars.tenpo_cd ++ "' AND prd_cd IN (" ++ vars.prd_cds joinBy "," ++ ")" ]]>
-        </ee:set-payload>
-    </ee:message>
-</ee:transform>
-
-<db:select doc:name="Run Query" config-ref="Database_Config">
-    <db:sql>#[payload]</db:sql>
-</db:select>
+Sub SplitAndReposition()
+    Dim rng As Range
+    Dim cell As Range
+    Dim splitValues As Variant
+    Dim i As Integer
+    
+    ' 変換したいセルを指定
+    Set rng = Range("A1")
+    
+    ' 改行でテキストを分割
+    splitValues = Split(rng.Value, vbLf)
+    
+    ' 分割したテキストを列に展開
+    For i = LBound(splitValues) To UBound(splitValues)
+        rng.Offset(0, i).Value = splitValues(i)
+    Next i
+End Sub
