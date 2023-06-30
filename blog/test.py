1b6336e19980413a4d9074fa355dd8cc2a603372
ggjp <54991419+ggjp@users.noreply.github.com>
2023-06-30 09:24:26 +0900

    Update test.py

--- blog/test.py
+++ blog/test.py
@@ -5,6 +5,7 @@ Sub SplitAndReposition()
     Dim splitValues As Variant
     Dim i As Integer
     Dim lastRow As Long
+    Dim output As String
 
     Set ws = ThisWorkbook.Sheets("Sheet1")  ' シート名を適切に変更してください
     lastRow = ws.Cells(ws.Rows.Count, "B").End(xlUp).Row  ' B列の最終行を取得
@@ -16,10 +17,17 @@ Sub SplitAndReposition()
         If cell.Value <> "" Then  ' B列のセルにデータが入力されている場合だけ処理を行う
             splitValues = Split(cell.Value, vbLf)
 
+            output = ""
             For i = LBound(splitValues) To UBound(splitValues)
-                ' コピー先をL列に設定
-                ws.Cells(cell.Row, "L").Offset(0, i).Value = splitValues(i)
+                ' 改行で分割したテキストをカンマで結合
+                output = output & splitValues(i) & ","
             Next i
+
+            ' 末尾の余分なカンマを削除
+            output = Left(output, Len(output) - 1)
+
+            ' 結果をL列に出力
+            ws.Cells(cell.Row, "L").Value = output
         End If
     Next cell
 End Sub
