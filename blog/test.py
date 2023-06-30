ba62ca89f3fb5451a191ad5af532afbba761b749
ggjp <54991419+ggjp@users.noreply.github.com>
2023-06-30 20:57:47 +0900

    Update test.py

--- blog/test.py
+++ blog/test.py
@@ -1,21 +1,21 @@
-<ee:transform doc:name="Transform Message" >
+<ee:transform doc:name="Prepare SQL Query" doc:id="d3b81e0a-75ad-4f66-8ef6-9885773d2e1b">
     <ee:message>
-        <ee:set-payload><![CDATA[
-            %dw 2.0
+        <ee:set-payload>
+            #[%dw 2.0
             output application/java
             ---
             {
                 'tenpo_cd': payload.tenpo_cd,
-                'prd_cd': payload.prd_cd map (product_code) -> '\'' ++ product_code ++ '\'' joinBy ','
-            }
-        ]]></ee:set-payload>
+                'prd_cd': "(" ++ (payload.prd_cd map '\'' ++ $ ++ '\'' joinBy ',') ++ ")"
+            }]
+        </ee:set-payload>
     </ee:message>
 </ee:transform>
 
 <db:select doc:name="Select" doc:id="8d88c75a-3062-4b33-bc6a-43f800aa0cb3" config-ref="Database_Config">
     <db:sql>
         <![CDATA[
-            SELECT * FROM products WHERE tenpo_cd = :tenpo_cd AND product_code IN (:prd_cd)
+            SELECT * FROM products WHERE tenpo_cd = :tenpo_cd AND product_code IN :prd_cd
         ]]>
     </db:sql>
     <db:input-parameters>
