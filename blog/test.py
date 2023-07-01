b361c7f71f86157a4bea9ba41c30ff19071ddc4d
ggjp <54991419+ggjp@users.noreply.github.com>
2023-07-01 12:09:50 +0900

    Update test.py

--- blog/test.py
+++ blog/test.py
@@ -1,24 +1,32 @@
 <mule xmlns:ee="http://www.mulesoft.org/schema/mule/ee/core" xmlns:http="http://www.mulesoft.org/schema/mule/http"
-      xmlns:db="http://www.mulesoft.org/schema/mule/db" xmlns="http://www.mulesoft.org/schema/mule/core"
-      xmlns:doc="http://www.mulesoft.org/schema/mule/documentation" 
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://www.mulesoft.org/schema/mule/core http://www.mulesoft.org/schema/mule/core/current/mule.xsd
-      http://www.mulesoft.org/schema/mule/http http://www.mulesoft.org/schema/mule/http/current/mule-http.xsd
-      http://www.mulesoft.org/schema/mule/db http://www.mulesoft.org/schema/mule/db/current/mule-db.xsd">
+    xmlns:db="http://www.mulesoft.org/schema/mule/db" xmlns="http://www.mulesoft.org/schema/mule/core"
+    xmlns:doc="http://www.mulesoft.org/schema/mule/documentation"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.mulesoft.org/schema/mule/core http://www.mulesoft.org/schema/mule/core/current/mule.xsd
+        http://www.mulesoft.org/schema/mule/http http://www.mulesoft.org/schema/mule/http/current/mule-http.xsd
+        http://www.mulesoft.org/schema/mule/db http://www.mulesoft.org/schema/mule/db/current/mule-db.xsd">
 
     <flow name="exampleFlow">
-        <http:listener doc:name="Listener" config-ref="HTTP_Listener_config" path="/example"/>
-        <set-variable variableName="inputArray" value="#[payload.aaa]" doc:name="Set Variable"/>
+        <http:listener doc:name="Listener" config-ref="HTTP_Listener_config" path="/example" />
+        <ee:transform doc:name="Transform Payload">
+            <ee:message>
+                <ee:set-payload><![CDATA[%dw 2.0
+output application/java
+---
+{
+  "tenpo_cd": payload.tenpo_cd,
+  "prd_cd": payload.prd_cd
+}]]></ee:set-payload>
+            </ee:message>
+        </ee:transform>
+        <set-variable variableName="placeholders" value="#[payload.prd_cd map ((item, index) -> ':prd_cd' ++ index)]" doc:name="Set Placeholders"/>
         <db:select doc:name="Select" config-ref="Database_Config">
-            <db:sql>
-                <![CDATA[
-                SELECT * FROM products WHERE column IN (
-                    <foreach collection="#[vars.inputArray]" separator="," >?</foreach>
-                )
-                ]]>
-            </db:sql>
+            <db:sql><![CDATA[
+                SELECT * FROM products WHERE tenpo_cd = :tenpo_cd AND product_code IN (:prd_cd)
+            ]]></db:sql>
             <db:input-parameters>
-                <db:foreach config-ref="Database_Config" collection="#[vars.inputArray]">
+                <db:input-parameter key="tenpo_cd" value="#[payload.tenpo_cd]" />
+                <db:foreach config-ref="Database_Config" collection="#[payload.prd_cd]">
                     <db:dynamic-input-parameter>
                         <ee:expression><![CDATA[#[payload]]]></ee:expression>
                     </db:dynamic-input-parameter>
