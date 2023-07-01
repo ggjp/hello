ef0c6303c50b1390e42ad326a622df32846f8296
ggjp <54991419+ggjp@users.noreply.github.com>
2023-07-01 12:42:33 +0900

    Update test.py

--- blog/test.py
+++ blog/test.py
@@ -1,11 +1,38 @@
-mvn process-classes -nsu -Dmaven.test.skip=true
-<plugins>
-    <plugin>
-        <groupId>org.apache.maven.plugins</groupId>
-        <artifactId>maven-compiler-plugin</artifactId>
-        <version>3.8.1</version>
-        <configuration>
-            <skip>true</skip>
-        </configuration>
-    </plugin>
-</plugins>
+<mule xmlns:ee="http://www.mulesoft.org/schema/mule/ee/core" xmlns:http="http://www.mulesoft.org/schema/mule/http"
+    xmlns:db="http://www.mulesoft.org/schema/mule/db" xmlns="http://www.mulesoft.org/schema/mule/core"
+    xmlns:doc="http://www.mulesoft.org/schema/mule/documentation"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.mulesoft.org/schema/mule/core http://www.mulesoft.org/schema/mule/core/current/mule.xsd
+        http://www.mulesoft.org/schema/mule/http http://www.mulesoft.org/schema/mule/http/current/mule-http.xsd
+        http://www.mulesoft.org/schema/mule/db http://www.mulesoft.org/schema/mule/db/current/mule-db.xsd">
+
+    <flow name="exampleFlow">
+        <http:listener doc:name="Listener" config-ref="HTTP_Listener_config" path="/example" />
+        <ee:transform doc:name="Transform Payload">
+            <ee:message>
+                <ee:set-payload><![CDATA[%dw 2.0
+output application/java
+---
+{
+  "tenpo_cd": payload.tenpo_cd,
+  "prd_cd": payload.prd_cd
+}]]></ee:set-payload>
+            </ee:message>
+        </ee:transform>
+        <set-variable variableName="placeholders" value="#[payload.prd_cd map ((item, index) -> ':prd_cd' ++ (index + 1))]" doc:name="Set Placeholders"/>
+        <db:select doc:name="Select" config-ref="Database_Config">
+            <db:sql><![CDATA[
+                SELECT * FROM products WHERE tenpo_cd = :tenpo_cd AND product_code IN (:prd_cd)
+            ]]></db:sql>
+            <db:input-parameters>
+                <db:input-parameter key="tenpo_cd" value="#[payload.tenpo_cd]" />
+                <db:foreach config-ref="Database_Config" collection="#[payload.prd_cd]">
+                    <db:dynamic-input-parameter>
+                        <ee:expression><![CDATA[#[payload]]]></ee:expression>
+                    </db:dynamic-input-parameter>
+                </db:foreach>
+            </db:input-parameters>
+        </db:select>
+    </flow>
+
+</mule>
