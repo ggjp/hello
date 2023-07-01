663611b30731a9ba220cbf6a2a1bbf8adfe74bf7
ggjp <54991419+ggjp@users.noreply.github.com>
2023-07-01 12:18:02 +0900

    Update test.py

--- blog/test.py
+++ blog/test.py
@@ -1,1 +1,11 @@
 mvn process-classes -nsu -Dmaven.test.skip=true
+<plugins>
+    <plugin>
+        <groupId>org.apache.maven.plugins</groupId>
+        <artifactId>maven-compiler-plugin</artifactId>
+        <version>3.8.1</version>
+        <configuration>
+            <skip>true</skip>
+        </configuration>
+    </plugin>
+</plugins>
