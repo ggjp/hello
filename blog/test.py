12091099da17dad6c67adc766889fa9e4805a3e9
ggjp <54991419+ggjp@users.noreply.github.com>
2023-07-01 10:42:07 +0900

    Update test.py

--- blog/test.py
+++ blog/test.py
@@ -1,13 +1,1 @@
-<set-variable variableName="prd_cd" value="#[payload.prd_cd joinBy "','"]" doc:name="Variable" />
-
-<db:select doc:name="Select" doc:id="8d88c75a-3062-4b33-bc6a-43f800aa0cb3" config-ref="Database_Config">
-    <db:sql>
-        <![CDATA[
-        SELECT * FROM products WHERE tenpo_cd = :tenpo_cd AND product_code IN (#[vars.prd_cd])
-        ]]>
-    </db:sql>
-    <db:input-parameters>
-        <db:input-parameter key="tenpo_cd" value="#[payload.tenpo_cd]" />
-        <db:input-parameter key="prd_cd" value="#[vars.prd_cd]" />
-    </db:input-parameters>
-</db:select>
+<set-variable variableName="placeholders" value="#[(1 to vars.inputArray size) map ('?') 
