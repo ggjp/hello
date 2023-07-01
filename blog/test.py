0e304c646c9c23cff51cef446e74023ad79065fc
ggjp <54991419+ggjp@users.noreply.github.com>
2023-07-01 11:42:17 +0900

    Update test.py

--- blog/test.py
+++ blog/test.py
@@ -1,12 +1,15 @@
 <set-variable variableName="inputArray" value="#[payload.aaa]" doc:name="Set Variable"/>
-<set-variable variableName="placeholders" value="#[vars.inputArray collect ('?' as String)]" doc:name="Set Placeholders"/>
 <db:select doc:name="Select" config-ref="Database_Config">
     <db:sql>
         <![CDATA[
-        SELECT * FROM products WHERE column IN (#[vars.placeholders joinBy ','])
+        SELECT * FROM products WHERE column IN (
+            <foreach collection="#[vars.inputArray]" separator="," >?</foreach>
+        )
         ]]>
     </db:sql>
     <db:input-parameters>
-        <db:input-parameter key="inputArray" value="#[vars.inputArray]" />
+        <db:foreach config-ref="Database_Config" collection="#[vars.inputArray]">
+            <db:input-parameter value="#[payload]" />
+        </db:foreach>
     </db:input-parameters>
 </db:select>
