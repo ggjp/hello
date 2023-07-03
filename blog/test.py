01ddff89a9d93793e1ec771f5939071dd4d34c37
ggjp <54991419+ggjp@users.noreply.github.com>
2023-07-03 12:40:33 +0900

    Update test.py

--- blog/test.py
+++ blog/test.py
@@ -50,3 +50,21 @@ payload]]></ee:set-payload>
         </ee:transform>
     </flow>
 </mule>
+<ee:transform doc:name="Generate dynamic in-clause and inputParameters" doc:id="8f6a02fd-4bdf-40e8-8c90-c23d39510944">
+    <ee:message />
+    <ee:variables >
+        <ee:set-variable variableName="tenpo_cd" ><![CDATA[%dw 2.0
+output application/java
+---
+payload.tenpo_cd]]></ee:set-variable>
+        <ee:set-variable variableName="inClause" ><![CDATA[%dw 2.0
+output application/java
+---
+(payload.prd_cd map (val, index) -> ":arg" ++ index) joinBy ", "]]></ee:set-variable>
+        <ee:set-variable variableName="inputParameters" ><![CDATA[%dw 2.0
+output application/java
+---
+(payload.prd_cd map (val, index) -> ("arg" ++ index) -> val) 
+    ++ {"tenpo_cd": vars.tenpo_cd as String}]]></ee:set-variable>
+    </ee:variables>
+</ee:transform>
