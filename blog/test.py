17ab76c5c88fb55822acb6b768e8a9050f9ee307
ggjp <54991419+ggjp@users.noreply.github.com>
2023-07-03 13:43:05 +0900

    Update test.py

--- blog/test.py
+++ blog/test.py
@@ -1,18 +1,39 @@
-<ee:transform doc:name="Generate dynamic in-clause and inputParameters" doc:id="8f6a02fd-4bdf-40e8-8c90-c23d39510944">
-    <ee:message />
-    <ee:variables >
-        <ee:set-variable variableName="tenpo_cd" ><![CDATA[%dw 2.0
+            </ee:message>
+            <ee:variables >
+                <ee:set-variable variableName="tenpo_cd" ><![CDATA[%dw 2.0
 output application/java
 ---
 payload.tenpo_cd]]></ee:set-variable>
-        <ee:set-variable variableName="inClause" ><![CDATA[%dw 2.0
+                <ee:set-variable variableName="prd_cd" ><![CDATA[%dw 2.0
 output application/java
 ---
-(payload.prd_cd map (val, index) -> ":arg" ++ (index as String)) joinBy ", "]]></ee:set-variable>
-        <ee:set-variable variableName="inputParameters" ><![CDATA[%dw 2.0
+payload.prd_cd]]></ee:set-variable>
+                <ee:set-variable variableName="inClause" ><![CDATA[%dw 2.0
 output application/java
 ---
-(payload.prd_cd map (val, index) -> {("arg" ++ (index as String)) : val})
-    ++ {"tenpo_cd": vars.tenpo_cd}]]></ee:set-variable>
-    </ee:variables>
-</ee:transform>
+(payload.prd_cd map ":prd_cd$$") joinBy ", "]]></ee:set-variable>
+                <ee:set-variable variableName="inputParameters" ><![CDATA[%dw 2.0
+output application/java
+---
+{
+  tenpo_cd: payload.tenpo_cd,
+  (payload.prd_cd mapObject (value, key) -> {
+    ("prd_cd$$" ++ (key as String)): value
+  })
+}]]></ee:set-variable>
+            </ee:variables>
+        </ee:transform>
+        <db:select doc:name="Select" doc:id="a4bafc22-6a69-44d5-8ba4-3949573bcd21" config-ref="Database_Config">
+            <db:sql>#["SELECT * FROM your_table WHERE tenpo_cd = :tenpo_cd AND prd_cd IN (" ++ vars.inClause ++ ")"]</db:sql>
+            <db:input-parameters><![CDATA[#[vars.inputParameters]]]></db:input-parameters>
+        </db:select>
+        <ee:transform doc:name="Transform Message" doc:id="fa970ecc-fe9d-49df-a965-604dbe5fd52e" >
+            <ee:message >
+                <ee:set-payload ><![CDATA[%dw 2.0
+output application/json
+---
+payload]]></ee:set-payload>
+            </ee:message>
+        </ee:transform>
+    </flow>
+</mule>
