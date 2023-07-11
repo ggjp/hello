a8298388b8508c6e375043d6fed72241344d9a93
ggjp <54991419+ggjp@users.noreply.github.com>
2023-07-11 15:39:45 +0900

    Update test.py

--- blog/test.py
+++ blog/test.py
@@ -1,4 +1,4 @@
 <ee:set-variable variableName="inClause"><![CDATA[%dw 2.0
 output application/java
 ---
-payload map ((item) -> "'" ++ (if (item is :number) "'" ++ item as String ++ "'" else item) ++ "'") reduce ((item, accumulator) -> accumulator ++ ", " ++ item)]]></ee:set-variable>
+payload map ("'" ++ $ ++ "'") reduce ((item, accumulator) -> accumulator ++ ", " ++ item)]]></ee:set-variable>
