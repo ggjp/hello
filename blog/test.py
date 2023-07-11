7426b170e93fb4362567c467e5e2cc88b8a21fb5
ggjp <54991419+ggjp@users.noreply.github.com>
2023-07-11 13:42:44 +0900

    Update test.py

--- blog/test.py
+++ blog/test.py
@@ -1,1 +1,19 @@
-new_results = [{k: str(v) if k != 'no' else v for k, v in row.items()} for row in results]
+<ee:transform doc:name="Generate dynamic in-clause" doc:id="8f6a02fd-4bdf-40e8-8c90-c23d39510944">
+    <ee:message>
+    </ee:message>
+    <ee:variables >
+        <ee:set-variable variableName="inClause" ><![CDATA[%dw 2.0
+output application/java
+---
+(payload map "'" ++ $ ++ "'" reduce ((item, accumulator = "") -> accumulator ++ (if (accumulator != "") ", " else "") ++ "'" ++ item ++ "'"))]]></ee:set-variable>
+    </ee:variables>
+</ee:transform>
+<ee:transform doc:name="Generate dynamic inputParameters" doc:id="bb25debf-54a2-4a30-a670-346939c8a667">
+    <ee:message />
+    <ee:variables>
+        <ee:set-variable variableName="inputParameters" ><![CDATA[%dw 2.0
+output application/java
+---
+payload map {"arg$$" : "'" ++ $ ++ "'"} reduce ((item, accumulator = {}) -> item ++ accumulator)]]></ee:set-variable>
+    </ee:variables>
+</ee:transform>
